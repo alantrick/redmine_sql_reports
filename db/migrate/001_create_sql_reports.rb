cb9ca8be5fe5550ddd0c612695ab297bfd7acbdb
Alan Trick <alantrick@gmail.com>
2010-10-08 14:21:58 -0700

    renamed a class that I missed in the installer

--- db/migrate/001_create_sql_reports.rb
+++ db/migrate/001_create_sql_reports.rb
@@ -1,7 +1,7 @@
 # Copyright (C) 2010 Alan Trick, Trinity Western University
 # see COPYING for license
 
-class CreateReports < ActiveRecord::Migration
+class CreateSqlReports < ActiveRecord::Migration
   def self.up
     create_table :sql_reports do |t|
       t.column :title, :text
