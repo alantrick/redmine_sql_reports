4c641e938ff5561171b768f28d2bca139d3e65a8
Alan Trick <alantrick@gmail.com>
2010-10-19 16:03:53 -0700

    Added report deletion

--- app/controllers/sql_reports_controller.rb
+++ app/controllers/sql_reports_controller.rb
@@ -9,7 +9,8 @@ class SqlReportsController < ApplicationController
   end
   
   def show
-    @report = SqlReport.find(params[:id])
+    render_404 if !SqlReport.exists? params[:id]
+    @report = SqlReport.find params[:id]
     args = {'user_id' => session[:user_id]}
     @error = "The report returned no results"
     @table = []
@@ -22,13 +23,14 @@ class SqlReportsController < ApplicationController
   
   def edit
     render_403 if !User.current.admin
-    @report = SqlReport.find(params[:id])
+    render_404 if !SqlReport.exists? params[:id]
+    @report = SqlReport.find params[:id]
     if request.post? and @report.update_attributes(params[:report])
       flash[:notice] = l(:notice_successful_update)
       redirect_to :action => 'show', :id => @report
     end
   end
-   
+  
   def add
     render_403 if !User.current.admin
     @report = SqlReport.new params[:report]
@@ -37,5 +39,15 @@ class SqlReportsController < ApplicationController
       flash[:notice] = l(:notice_successful_update)
       redirect_to :action => 'show', :id => @report
     end
+  end
+  
+  def destroy
+    render_403 if !User.current.admin
+    # rails 405 wanabe
+    raise ActionController::NotImplemented if !request.post?
+    render_404 if !SqlReport.exists? params[:id]
+    SqlReport.delete params[:id]
+    flash[:notice] = l(:notice_successful_delete)
+    redirect_to :action => 'index'
   end
 end
--- app/helpers/sql_reports_helper.rb
+++ app/helpers/sql_reports_helper.rb
@@ -2,38 +2,43 @@
 # see COPYING for license
 
 module SqlReportsHelper
+  def link_to_if_admin *args
+    if User.current.admin
+      link_to *args
+    end
+  end
 
-    def report_header column
-      if column =~ /^(\w+)\_id$/
-        column = $1
-      end
-      return column.split(/[\s_]+/).map{|s| s.capitalize}.join ' '
+  def report_header column
+    if column =~ /^(\w+)\_id$/
+      column = $1
     end
-    
-    def report_cell column, data
-      if column =~ /^([A-Z]\w+)$/
-        return link_model $1, data
-      elsif column =~ /^(\w+)\_id$/
-        # go from under_scores to CamelCase
-        classname = $1.split('_').map{|s| s.capitalize}.join
-        return link_model classname, data
-      else
-        return data
-      end
+    return column.split(/[\s_]+/).map{|s| s.capitalize}.join ' '
+  end
+  
+  def report_cell column, data
+    if column =~ /^([A-Z]\w+)$/
+      return link_model $1, data
+    elsif column =~ /^(\w+)\_id$/
+      # go from under_scores to CamelCase
+      classname = $1.split('_').map{|s| s.capitalize}.join
+      return link_model classname, data
+    else
+      return data
     end
-    
-    def link_model classname, data
-      begin
-        cls = Kernel.const_get(classname)
-        d = cls.find(data)
-        # using cls.table_name for the controller name is a hack,
-        # but I think it works and I don't know a better way to access the
-        # controller
-        return link_to d.to_s, {:controller => cls.table_name, :action => 'show', :id => data}
-      rescue NameError, LoadError, NoMethodError
-        return data
-      end
+  end
+  
+  def link_model classname, data
+    begin
+      cls = Kernel.const_get(classname)
+      d = cls.find(data)
+      # using cls.table_name for the controller name is a hack,
+      # but I think it works and I don't know a better way to access the
+      # controller
+      return link_to d.to_s, {:controller => cls.table_name, :action => 'show', :id => data}
+    rescue NameError, LoadError, NoMethodError
+      return data
     end
+  end
 end
 
 
